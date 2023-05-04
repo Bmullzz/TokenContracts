960c7fbf5b67b156a2debea2605e4fc9d6e935e3
Brian Mullin <brmullin@email.msmary.edu>
2023-05-04 18:19:16 -0400

    add Godmode test file

---
+++ test/GodModeTest.js
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ethers } from "hardhat";
+import { Signer } from "ethers";
+
+import { assert } from "chai";
+
+describe("GodMode", () => { 
+    
+
+});
