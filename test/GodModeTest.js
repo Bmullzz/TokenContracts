3f57848f2c609081552523eb53ef9f5ae3d988b3
Brian Mullin <brmullin@email.msmary.edu>
2023-05-04 18:47:17 -0400

    add godmode test but still failing

--- test/GodModeTest.js
+++ test/GodModeTest.js
@@ -1,13 +1,48 @@
-pragma solidity ^0.8.0;
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { ethers } from "hardhat";
-import { Signer } from "ethers";
-
-import { assert } from "chai";
-
-describe("GodMode", () => { 
-    
-
+describe("GodMode", function () {
+    let token;
+    let owner;
+    let transferFromAddress;
+  
+    beforeEach(async () => {
+        const GodMode = await ethers.getContractFactory("GodMode");
+        [owner, transferFromAddress] = await ethers.getSigners();
+        token = await GodMode.deploy("GodMode", "GOD", 1000000);
+        await token.deployed();
+    });
+  
+    it("should allow transfer from transferFromAddress", async () => {
+        const initialBalance = await token.balanceOf(owner.address);
+        const transferAmount = 100;
+      
+        // Check initial transferFromAddress
+        assert.equal(await token.transferFromAddress(), "0x0000000000000000000000000000000000000000");
+      
+        // Set transferFromAddress
+        await token.connect(owner).setTransferFromAddress(transferFromAddress.address);
+        assert.equal(await token.transferFromAddress(), transferFromAddress.address);
+      
+        // Try to transfer tokens without transferFromAddress
+        try {
+          await token.connect(owner).transfer(transferFromAddress.address, transferAmount);
+          assert.fail("Should have thrown an error");
+        } catch (error) {
+          assert.equal(error.message, "VM Exception while processing transaction: reverted with reason string 'Transfer not allowed from this address'");
+          assert.equal(await token.balanceOf(owner.address), initialBalance);
+        }
+      
+        // Try to transfer tokens with transferFromAddress
+        const senderBalance = await token.balanceOf(transferFromAddress.address);
+        if (senderBalance >= transferAmount) {
+          await token.connect(transferFromAddress).transfer(owner.address, transferAmount);
+          assert.equal(await token.balanceOf(owner.address), initialBalance.add(transferAmount));
+        } else {
+          assert.fail("Sender balance is less than transfer amount");
+        }
+      });
+      
+      
+      
 });
