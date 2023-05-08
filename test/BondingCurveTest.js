87ba3e5b021143f45f3cf73c2e64bc01e1e02afd
Brian Mullin <brmullin@email.msmary.edu>
2023-05-07 22:35:02 -0400

    add a test to BondingCurveTest

--- test/BondingCurveTest.js
+++ test/BondingCurveTest.js
@@ -1,6 +1,23 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
-describe('SanctionsToken contract', () => { 
-    
+describe('BondingCurve', () => { 
+
+    let token;
+    let tokenSale;
+    let owner;
+    let buyer;
+
+    beforeEach(async () => {
+        const BCToken = await ethers.getContractFactory("BCToken");
+        token = await BCToken.deploy("BCToken", "BCT", ethers.utils.parseEther("1000"));
+        await token.deployed();
+
+        const BondingCurve = await ethers.getContractFactory("BondingCurve");
+        [owner, buyer] = await ethers.getSigners();
+        tokenSale = await BondingCurve.deploy(token.address, ethers.utils.parseEther("1"), 100);
+        await tokenSale.deployed();
+
+        await token.connect(owner).approve(tokenSale.address, ethers.utils.parseEther("1000"));
+    });
 });
