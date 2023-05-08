525d06df3d24eccbaf5e2de9c644e4a08b588e4f
Brian Mullin <brmullin@email.msmary.edu>
2023-05-07 21:53:48 -0400

    add method stubs to BondingCurve

--- contracts/BondingCurve.sol
+++ contracts/BondingCurve.sol
@@ -4,6 +4,53 @@ pragma solidity ^0.8.0;
 ///@title Token sale and buyback with bonding curve.
 ///@author Brian Mullin
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC1363/IERC1363Receiver.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract BondingCurve {
-    
+
+    ERC20 public token;
+    uint256 public reserve;
+    uint256 public rate;
+    uint256 public scale;
+
+    constructor(address _token, uint256 _reserve, uint256 _rate, uint256 _scale) {
+        token = ERC20(_token);
+        reserve = _reserve;
+        rate = _rate;
+        scale = _scale;
+    }
+
+    function buy() {
+
+    }
+
+    function sell() {
+
+    }
+
+    function getCost() {
+
+    }
+
+    function getGain() {
+
+    }
+
+    function onTransferRecieved() {
+
+    }
+
+    function setRate() {
+
+    }
+
+    function setScale() {
+
+    }
+
+    function withdraw() {
+        
+    }
 }
